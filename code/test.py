2c8d2c74058bea43b34c2537c0f4c3b85c703dbf
Gaurav Paruthi <gparuthi@si-rollercoaster-mp.si.umich.edu>
2013-03-13 00:16:35 -0400

    made code efficient

--- code/test.py
+++ code/test.py
@@ -1,55 +1,33 @@
 import csv
 import ipdb
 
-def readcsvpres(f):
+def readcsv(f, cid_row, text_row, master_row, coname_row):
 	d = {}
 	n=0
 	for row in csv.reader(f,delimiter='\t'):
-		c_id = row[3]
-		text = row[5]
-		coname = row[2]
+		c_id = row[cid_row]
+		text = row[text_row]
+		coname = row[coname_row]
 		# ipdb.set_trace()
-		master = row[8]
-
-		json = {'text':text, 'master':master, 'coname' : coname}
-		if c_id in d:
-			d[c_id].append(json)
-		else:
-			d[c_id] = [json]
-		if n%1000 == 0:
-			print n
-
+		master = row[master_row]
 		n+=1
-	return d
 
-
-def readcsvqa(f):
-	d = {}
-	n=0
-	for row in csv.reader(f,delimiter='\t'):
-		c_id = row[5]
-		text = row[8]
-		coname = row[2]
-		# ipdb.set_trace()
-		master = row[10]
-
-		json = {'text':text, 'master':master, 'coname' : coname}
 		if c_id in d:
-			d[c_id].append(json)
+			if master in d[c_id]:
+				d[c_id][master] += ' ' + text
+			else:
+				d[c_id][master] = text
 		else:
-			d[c_id] = [json]
-		if n%1000 == 0:
+			d[c_id] = {}
+
+		if n%100000 == 0:
 			print n
-
-		n+=1
 	return d
-
-
 
 f = open('../full_dataset_matched_modifiedtabPRES.txt')
 
-presd = readcsvpres(f)
+presd = readcsv(f, 3, 5, 8, 2)
 
 f = open('../full_dataset_matched_modifiedtabQA.txt')
 
-qad = readcsvqa(f)
+qad = readcsv(f, 5, 7, 10, 2)
